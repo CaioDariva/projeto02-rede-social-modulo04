c6c1051c9f9b49d5873a3095f5ca45584305df9d
Caio Dariva <cgmalavazidariva@gmail.com>
2021-10-12 11:19:34 -0300

    fix app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { PrismaService } from './prisma/prisma.service';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UsersModule, AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, PrismaService, AuthService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
