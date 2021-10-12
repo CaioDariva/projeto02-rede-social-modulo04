5c76fac3f3f67b4aab71fa1436057789db89ecb5
Caio Dariva <cgmalavazidariva@gmail.com>
2021-10-12 19:00:05 -0300

    fix import in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
-import { LoginDto, AuthResponse } from './auth.dto';
+import { LoginDto, AuthResponse } from './dto/auth.dto';
 import { Request } from 'express';
 
 @Controller('auth')
