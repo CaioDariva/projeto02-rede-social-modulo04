c2debc05dc20f8dd1dd5b54d25998c408a91c84a
Caio Dariva <cgmalavazidariva@gmail.com>
2021-10-12 10:35:13 -0300

    auth, jwt, guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './jwt.constants';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthController } from './auth.controller';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, PrismaService],
+  controllers: [AuthController],
+})
 export class AuthModule {}
